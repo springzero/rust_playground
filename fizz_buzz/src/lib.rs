fcbedd6935651c1542343cdd4639f0e948cd5686
spring <spring.ding@aisenz.com>
2024-03-11 19:09:26 +0800

    fizz_buzz

---
+++ fizz_buzz/src/lib.rs
@@ -0,0 +1,45 @@
+
+pub fn play_game(n: u32, print: bool) {
+    // let result = fizz_buzz(n);
+    let result = fizz_buzz_fibonacci(n);
+    if print {
+        println!("{result}");
+    }
+}
+
+pub fn fizz_buzz(n: u32) -> String {
+    match (n % 3, n % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        (_, _) => n.to_string(),
+    }
+}
+
+pub fn fizz_buzz_fibonacci(n: u32) -> String {
+    if is_fibonacci_number(n) {
+        "Fibonacci".to_string()
+    } else {
+        match (n % 3, n % 5) {
+            (0, 0) => "FizzBuzz".to_string(),
+            (0, _) => "Fizz".to_string(),
+            (_, 0) => "Buzz".to_string(),
+            (_, _) => n.to_string(),
+        }
+    }
+}
+
+fn is_fibonacci_number(n: u32) -> bool {
+    for i in 0..=n {
+        let (mut previous, mut current) = (0, 1);
+        while current < i {
+            let next = previous + current;
+            previous = current;
+            current = next;
+        }
+        if current == n {
+            return true;
+        }
+    }
+    false
+}
---
+++ fizz_buzz/src/main.rs
@@ -0,0 +1,7 @@
+use fizz_buzz::play_game;
+
+fn main() {
+    for i in 1..=100 {
+        play_game(i, true);
+    }
+}
